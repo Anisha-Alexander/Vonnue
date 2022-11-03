bd0b266f93fdc3e78b119ab538e6ddc4e2ab7953
Anisha Alexander <anisha.alexander@vonnue.com>
2022-11-03 17:04:45 +0530

    feat: Transcript by Mosh

---
+++ Mosh-tsc/src/index.js
@@ -0,0 +1,15 @@
+// let age : number =20;
+// if (age <50)
+//     age +=10
+// let sales: number = 123_456_789;
+// let course='Typescript'
+// let published_level=true
+// let level
+//  function render(document:any){
+//     console.log(document)
+//  }
+// let anyarr:any[]=[1,2,'3']
+// let numberarr:number[]=[1,2,3,4,]
+var user = [1, 'Mosh'];
+user.push('hello');
+console.log(user);
