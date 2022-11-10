b329c7855bcbaa17fcbb858c936ed35c02d75e4f
Anisha Alexander <anisha.alexander@vonnue.com>
2022-11-10 09:53:30 +0530

    feat: modified article

--- etsy-reactts-clone/src/components/Article/Article.tsx
+++ etsy-reactts-clone/src/components/Article/Article.tsx
@@ -4,23 +4,23 @@ import "./Article.css"
 import ImgCards from '../Main/ImgCards'
 import { BiRightArrowAlt } from "react-icons/bi"
 
-const Article : React.FC =() =>{
-    const cardsArr= data.article.imgTextBox
-  return (
-    <article className='article'>
-        <div className='article-label-card'>
-            <h2>{data.article.artlabel}</h2>
-            <BiRightArrowAlt/>
-        </div>
-        <div className='article-img-cards-cont'>
-            {
-                cardsArr.map(item =>(
-                    <ImgCards imgUrl={item.imgURL} title={item.title} desc= {item.desc} isPara/>
-                ))
-            }
-        </div>
-    </article>
-  )
+const Article: React.FC = () => {
+    const cardsArr = data.article.imgTextBox
+    return (
+        <article className='article'>
+            <div className='article-label-card'>
+                <h2>{data.article.artlabel}</h2>
+                <BiRightArrowAlt />
+            </div>
+            <div className='article-img-cards-cont'>
+                {
+                    cardsArr.map(item => (
+                        <ImgCards imgUrl={item.imgURL} title={item.title} desc={item.desc} isPara />
+                    ))
+                }
+            </div>
+        </article>
+    )
 }
 
 export default Article
