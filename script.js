e2ee7d184ebe8cb70258f47a58b3b0a4ce74baf8
Anisha Alexander <anisha.alexander@vonnue.com>
2022-11-22 09:26:25 +0530

    feat: updated mother page

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const websiteContEl = document.querySelector('.website-container')
 console.log(websiteContEl)
 
 const webarr= data.website
-const webfunct=webarr.forEach((item) =>{
+webarr.forEach((item) =>{
     websiteContEl.innerHTML +=`<div class="anchor-element-card">
         <a href="${item.url}">${item.name}</a>                
     </div>`
