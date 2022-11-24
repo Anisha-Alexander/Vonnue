f5348ccf07ef6b8720449ff3042e473fe1508435
Anisha Alexander <anisha.alexander@vonnue.com>
2022-11-24 09:35:15 +0530

    Feat: styles for motherpage

--- script.js
+++ script.js
@@ -24,8 +24,27 @@ webarr.forEach((item) =>{
             </a>           
         </div>
     </div>`
-
 })
+
+const cards  = document.querySelectorAll('.anchor-element-card')
+console.log(cards)
+window.addEventListener('scroll',checkBoxes)
+
+checkBoxes()
+
+function checkBoxes(){
+    const triggerBottom = window.innerHeight/ 5 * 4
+    cards.forEach(card => {
+        const cardTop = card.getBoundingClientRect().top
+
+        if(cardTop<triggerBottom){
+            card.classList.add('show')
+        }
+        else{
+            card.classList.remove('show')
+        }
+    })
+}
 
 
 
